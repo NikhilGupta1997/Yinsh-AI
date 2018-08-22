8d2d3497101383f6c539e3e069d46ff8bc05f27b
13dexter <singhrishubh13@gmail.com>
2018-08-22 12:17:24 +0530

    Adding starting code for game.py

---
+++ game.py
@@ -0,0 +1,15 @@
+#communicates with the js file using selenium to integrate the yinsh sim and the client -server architecture
+
+from selenium import webdriver
+
+driver = webdriver.Firefox()
+# we'll have to make this relative to work for everyone
+driver.get("file:/home/keshav/ta/ai/yinsh/Yinsh.html")
+
+el=driver.find_elements_by_id("PieceLayer")
+
+action = webdriver.common.action_chains.ActionChains(driver)
+action.move_to_element_with_offset(el[0], 438, 438)
+action.click()
+action.perform()
+
